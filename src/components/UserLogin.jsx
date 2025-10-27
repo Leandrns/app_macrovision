711d5f1acd038e3dac1b379fac912ba3946aaad7
Leandrns <leandro2005souza@gmail.com>
2025-10-27 19:48:13 -0300

    feat: ícone de ver/ocultar senha

--- src/components/UserLogin.jsx
+++ src/components/UserLogin.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/UserLogin.css';
 import LogoAzul from '../assets/logo_macrovision_p.svg'
 
 function UserLogin({ onBack, onLogin }) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="user-login-container">
       <img src={LogoAzul} alt="Macrovision Logo" className="login-logo" />
       <div className="user-login-box">
         <div className="login-header">
-            <button className="back-button" onClick={onBack}><i className="fa-solid fa-arrow-left"></i> Voltar</button>
+            <button className="back-button" onClick={onBack}>
+              <i className="fa-solid fa-arrow-left"></i> Voltar
+            </button>
         </div>
         
         <h2>Login</h2>
@@ -21,8 +29,16 @@ function UserLogin({ onBack, onLogin }) {
             <input type="text" id="crm" placeholder="CRM" required />
           </div>
           <div className="input-group">
-            <input type="password" id="password" placeholder="Senha" required />
-            <i className="fa-solid fa-eye-slash password-icon"></i>
+            <input 
+              type={showPassword ? "text" : "password"} 
+              id="password" 
+              placeholder="Senha" 
+              required 
+            />
+            <i 
+              className={`fa-solid ${showPassword ? 'fa-eye' : 'fa-eye-slash'} password-icon`}
+              onClick={togglePasswordVisibility}
+            ></i>
           </div>
           <a href="#" className="forgot-password">Trocar senha...</a>
           <button type="submit" className="signin-btn">Sign In</button>
